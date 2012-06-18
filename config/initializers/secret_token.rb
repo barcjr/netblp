faa75e78df021b1a4b242845564bf1382f7ab261
Hargobind S. Khalsa <khalsah@gmail.com>
2012-06-18 16:17:32 -0600

    Import rails app scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Netblp::Application.config.secret_token = '0d77c2aa5105ec17782fcea1151f1e99305278affc99b7dc94b4d20e7ec1f8109b1de8dbc3571cf1c538d504871c477899e89e422d3f477b362b47d0f23237e7'
