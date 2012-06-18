4b9f542fc844ae1beadd8e176c68afcd2a3ef779
Hargobind S. Khalsa <khalsah@gmail.com>
2012-06-18 16:25:36 -0600

    Add Band model

---
+++ app/models/band.rb
@@ -0,0 +1,41 @@
+class Band
+  attr_reader :name, :lower, :upper
+
+  def self.all
+    @all ||= load_csv
+  end
+
+  def self.for_frequency frequency
+    all.find{ |band| band.lower <= frequency && band.upper >= frequency }
+  end
+
+  def initialize values
+    values.each do |k, v|
+      self.send("#{k}=", v)
+    end
+  end
+
+  protected
+
+  def name= name
+    @name = name.to_s
+  end
+
+  def lower= lower
+    @lower = lower.to_f
+  end
+
+  def upper= upper
+    @upper = upper.to_f
+  end
+
+  def self.load_csv
+    require "csv"
+
+    path = File.join(Rails.root, "data/bands.csv")
+    options = {headers: true, header_converters: [:symbol]}
+
+    CSV.read(path, options).map{ |row| self.new row }
+  end
+end
+
